385a970d689bc6a1d00f9cd16bb4e948f5e78baa
jip0005 <jip0005@mix.wvu.edu>
2021-08-30 23:17:49 -0400

    Added navigation header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { BottomNavBarComponent } from './footer/bottom-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    BottomNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/footer/bottom-nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+    selector: 'app-bottom-nav-bar',
+    templateUrl: 'bottom-nav-bar.component.html'
+
+})
+
+export class BottomNavBarComponent {
+
+}
